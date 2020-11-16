13d9d0c57564ad7607fde63da7f95422e3fc5695
robert <robert@example.com>
2020-11-16 16:50:25 +0800

    [Cpp] Binary Search Tree : 1. implement minimum, maximum, removeMin, removeMax, remove feature.

--- Cpp/algorithm/BinarySearchTree.h
+++ Cpp/algorithm/BinarySearchTree.h
@@ -18,6 +18,13 @@ private:
             this->left = nullptr;
             this->right = nullptr;
         }
+
+        Node(Node* node) {
+            this->key = node->key;
+            this->value = node->value;
+            this->left = node->left;
+            this->right = node->right;
+        }
     };
 
 private:
@@ -80,6 +87,34 @@ public:
             if(node->right != nullptr)
                 q.push(node->right);
         }
+    }
+
+    Key minimum() {
+        assert( count != 0);
+        Node* minNode = __minimum(root);
+        return minNode->key;
+    }
+
+    Key maximum() {
+        assert( count != 0);
+        Node* maxNode = __maximum(root);
+        return maxNode->key;
+    }
+
+    void removeMin() {
+        if(root) {
+            root = __removeMin(root);
+        }
+    }
+
+    void removeMax() {
+        if(root) {
+            root = __removeMax(root);
+        }
+    }
+
+    void remove(Key key) {
+        root = __remove(root, key);
     }
 
 private:
@@ -153,6 +188,80 @@ private:
             __destroy(node->right);
             delete node;
             count--;
+        }
+    }
+
+    Node* __minimum(Node* node) {
+        if(node->left == nullptr)
+            return node;
+        return __minimum(node->left);
+    }
+
+    Node* __maximum(Node* node) {
+        if(node->right == nullptr)
+            return node;
+        return __maximum(node->right);
+    }
+
+    Node* __removeMin(Node* node) {
+        if(node->left == nullptr) {
+            Node* rightNode = node->right;
+            delete node;
+            count--;
+            return rightNode;
+        }
+
+        node->left = __removeMin(node->left);
+        return node;
+    }
+
+    Node* __removeMax(Node* node) {
+        if(node->right == nullptr) {
+            Node* leftNode = node->left;
+            delete node;
+            count--;
+            return leftNode;
+        }
+
+        node->right = __removeMax(node->right);
+        return node;
+    }
+
+    Node* __remove(Node* node, Key key) {
+        if(node == nullptr)
+            return nullptr;
+
+        if(key < node->key) {
+            node->left = __remove(node->left, key);
+            return node;
+        } else if(key > node->key) {
+            node->right = __remove(node->right, key);
+            return node;
+        } else {
+            if(node->left == nullptr) {
+                Node* rightNode = node->right;
+                delete node;
+                count--;
+                return rightNode;
+
+            }
+            if(node->right == nullptr) {
+                Node* leftNode = node->left;
+                delete node;
+                count--;
+                return leftNode;
+            }
+
+            /** node->left != NULL && node->right != NULL */
+            Node* successor = new Node( __minimum(node->right) );
+            count++;
+
+            successor->right = __removeMin(node->right);
+            successor->left = node->left;
+            delete node;
+            count--;
+
+            return successor;
         }
     }
 };
